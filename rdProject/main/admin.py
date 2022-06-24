69c8508ef17f42bd7487dd8ee18b2305d3b327fd
jycforest29 <103106183+jycforest29@users.noreply.github.com>
2022-06-24 18:06:12 +0900

    Delete rdProject directory

--- rdProject/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Sentence, Chunk
-
-# Register your models here.
-
-admin.site.register(Sentence)
-admin.site.register(Chunk)
--- rdProject/main/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import sentenceApiView
-
-urlpatterns = [
-    path('sentence/<str:dest>', sentenceApiView.as_view(), name = 'translate-sentence'),
-    # path('file/<str:dest>', fileApiView.as_view(), name = 'translate-file'),
-]
- 
